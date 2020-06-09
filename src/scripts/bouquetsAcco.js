108ceff375c093b36b9a8ff601c2cf01568f79c7
SDen4 <asw325@yandex.ru>
2020-06-09 23:19:24 +0500

    bouquets accordion

--- src/scripts/bouquetsAcco.js
+++ src/scripts/bouquetsAcco.js
@@ -4,17 +4,16 @@ const bouquetsNum = document.querySelectorAll(".bouquets__acco_item");
 const line = document.querySelector("#line");
 let move = 0;
 
-
 moveRight.addEventListener('click', () => {
     if(move > -bouquetsNum.length+1) {
         move -= 1;
         line.style.left = `${move*100}%`;
-    }
+    };
 });
 
 moveLeft.addEventListener('click', () => {
     if(move<0) {
         move += 1;
         line.style.left = `${move*100}%`;
-    }
+    };
 });
