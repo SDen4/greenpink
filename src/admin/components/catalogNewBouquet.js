a0e2a26a2921b03375e0960e2a1b30393e0bd93a
SDen4 <asw325@yandex.ru>
2020-06-27 23:04:50 +0500

    consolidation information in new bouquet form

--- src/admin/components/catalogNewBouquet.js
+++ src/admin/components/catalogNewBouquet.js
@@ -1,27 +1,47 @@
 import React, {Component} from 'react';
 
 class CatalogNewBouquet extends Component {
-    constructor(props) {
-        super(props)
+    state = {
+        bouquetName: "",
+        bouquetPrice: 0,
+        bouquetDescription: ""
     }
     render() {
         const {handleClickNewBouquet} = this.props;
         return (
             <div className={`${handleClickNewBouquet ? "catalog__new-bouquet_active" : "catalog__new-bouquet"}`}>
                 <h2 className="admin__subtitle">Add the new bouquet</h2>
-                <form className="admin__new-bouquet_form">
+                <form className="admin__new-bouquet_form" onSubmit={this.sendNewBouquet}>
                     <div className="admin__new-bouquet_form_part admin__new-bouquet_form_part_left">
                         <label className="admin__new-bouquet_form_label">
                             <div className="admin__new-bouquet_form_name">Name</div>
-                            <input placeholder="Enter the name of the bouquet"></input>
+                            <input 
+                                type="text"
+                                placeholder="Enter the name of the bouquet" 
+                                value={this.state.bouquetName} 
+                                onChange={this.handleChange} 
+                                name="bouquetName"
+                            ></input>
                         </label>
                         <label className="admin__new-bouquet_form_label">
                             <div className="admin__new-bouquet_form_name">Price</div>
-                            <input placeholder="Enter the price"></input>
+                            <input 
+                                type="number"
+                                placeholder="Enter the price" 
+                                value={this.state.bouquetPrice} 
+                                onChange={this.handleChange}
+                                name="bouquetPrice"
+                            ></input>
                         </label>
                         <label className="admin__new-bouquet_form_label">
                             <div className="admin__new-bouquet_form_name">Description</div>
-                            <textarea className="admin__new-bouquet_form_textarea" placeholder="Enter the description"></textarea>
+                            <textarea 
+                                className="admin__new-bouquet_form_textarea" 
+                                placeholder="Enter the description" 
+                                value={this.state.bouquetDescription} 
+                                onChange={this.handleChange}
+                                name="bouquetDescription"
+                            ></textarea>
                         </label>
                     </div>
                     <div className="admin__new-bouquet_form_part admin__new-bouquet_form_part_right">
@@ -38,6 +58,17 @@ class CatalogNewBouquet extends Component {
                 </form>
             </div>
         )
+    }
+    handleChange = (event) => {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+        this.setState({
+            [name]: value
+        })
+    }
+    sendNewBouquet = () => {
+        alert("New bouquet name: " + this.state.bouquetName + ", price: " + this.state.bouquetPrice + ", description: " + this. state.bouquetDescription)
     }
 };
 
